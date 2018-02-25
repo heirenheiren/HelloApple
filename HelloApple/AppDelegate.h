ce1b8f1d2579215cefe133c0ce40de67337a2431
Polin <Polin@zhoudemacbook.local>
2018-02-25 20:19:43 +0800

    Initial Commit

---
+++ HelloApple/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  HelloApple
+//
+//  Created by Polin on 2018/2/25.
+//  Copyright © 2018年 Polin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ HelloApple/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloApple
+//
+//  Created by Polin on 2018/2/25.
+//  Copyright © 2018年 Polin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
